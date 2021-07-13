702172c86264d2440cdc7b69e50166ca9455aa39
David Sanderson <david.sanderson@tombola.com>
2021-07-13 11:06:32 +0100

    reuse base implementation of methods if useDeepLinkFlag is false

--- src/ios/AppsFlyerX+AppController.m
+++ src/ios/AppsFlyerX+AppController.m
@@ -11,33 +11,39 @@
 @implementation AppDelegate (AppsFlyerX)
 BOOL useDeepLink;
 
--(id) init{
+- (id) init{
     if (self = [super init]) {
         id deepLinkFlag = [[NSBundle mainBundle] objectForInfoDictionaryKey:APPSFLYER_DEEPLINK_FLAG];
         useDeepLink = deepLinkFlag ? ![deepLinkFlag boolValue] : YES;
     }
-    return  self;
+    return self;
 }
 
-- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options{
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)openURL options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options{
     if(useDeepLink){
-        [[AppsFlyerAttribution shared] handleOpenUrl:url options:options];
+        [[AppsFlyerAttribution shared] handleOpenUrl:openURL options:options];
+        return YES;
+    } else {
+        return [super application:application openURL:openURL options:options];
     }
-    return YES;
 }
 
 - (BOOL)application:(UIApplication *)application continueUserActivity:(NSUserActivity *)userActivity restorationHandler:(void (^)(NSArray<id<UIUserActivityRestoring>> * _Nullable))restorationHandler{
     if(useDeepLink){
         [[AppsFlyerAttribution shared] continueUserActivity:userActivity restorationHandler:restorationHandler];
+        return YES;
+    } else {
+        return [super application:application continueUserActivity:userActivity restorationHandler:restorationHandler];
     }
-    return YES;
 }
 
--(BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation{
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation{
     if(useDeepLink){
         [[AppsFlyerAttribution shared] handleOpenUrl:url sourceApplication:sourceApplication annotation:annotation];
+        return YES;
+    } else {
+        return [super application:application openURL:url sourceApplication:sourceApplication annotation:annotation];
     }
-    return YES;
 }
 
 @end
